f0528e737e3743e4fa9ac0cdeb3481dc1230c2b4
Yann Diorcet <yann.diorcet@belledonne-communications.com>
2013-06-14 14:59:09 +0200

    Add define to config.h

--- gen_templates/config.h
+++ gen_templates/config.h
@@ -6,11 +6,13 @@
 \**********************************************************/
 
 #define FBSTRING_CompanyName "${FBSTRING_CompanyName}"
+#define FBSTRING_CompanyDomain "${FBSTRING_CompanyDomain}"
 #define FBSTRING_PLUGIN_VERSION "${FBSTRING_PLUGIN_VERSION}"
 #define FBSTRING_LegalCopyright "${FBSTRING_LegalCopyright}"
 #define FBSTRING_PluginFileName "${FBSTRING_PluginFileName}"
 #define FBSTRING_ProductName "${FBSTRING_ProductName}"
 #define FBSTRING_PluginName "${FBSTRING_PluginName}"
+#define FBSTRING_PluginDomain "${FBSTRING_PluginDomain}"
 
 // Format is Major.Minor.Patch.Build
 
